996d4206cf688a2749b4d6db990f8f8fded101b7
Abheek Basu <abheek94@gmail.com>
2024-01-30 20:36:27 -0500

    fix fire2

--- Sources/LaudspeakerCore/LaudspeakerCore.swift
+++ Sources/LaudspeakerCore/LaudspeakerCore.swift
@@ -206,7 +206,7 @@ public class LaudspeakerCore {
         
         let postData = parameters.data(using: .utf8)
         
-        var request = URLRequest(url: URL(string: (self.endpointUrl ?? "") + "/events")!,timeoutInterval: Double.infinity)
+        var request = URLRequest(url: URL(string: (self.endpointUrl ?? "") + "events")!,timeoutInterval: Double.infinity)
         //var request = URLRequest(url: URL(string: "https://api.laudspeaker.com/events/")!,timeoutInterval: Double.infinity)
         request.addValue("Api-Key " + (self.apiKey ?? ""), forHTTPHeaderField: "Authorization")
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
